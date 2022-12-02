4b1e785c6897c3f40a78a243efe19825ce4e279d
Menyhert Fatyol <menyhert.fatyol@gmail.com>
2022-12-02 15:08:41 +0100

    Add necessary requires, fix private methods to work on class

--- lib/door.rb
+++ lib/door.rb
@@ -1,32 +1,37 @@
-class Door
-  def self.check_lock_state(pin_num: 11)
-    RPi::GPIO.set_numbering :board
-    RPi::GPIO.setup pin_num, as: :input, pull: :up
+require 'rpi_gpio'
+require 'aws-sdk-dynamodb'
 
-    if RPi::GPIO.high? pin_num
-      state = 'open'
-    else
-      state = 'locked'
+class Door
+  class << self
+    def check_lock_state(pin_num: 11)
+      RPi::GPIO.set_numbering :board
+      RPi::GPIO.setup pin_num, as: :input, pull: :up
+
+      if RPi::GPIO.high? pin_num
+        state = 'open'
+      else
+        state = 'locked'
+      end
+
+      store_state(state)
+    ensure
+      RPi::GPIO.clean_up
     end
 
-    store_state(state)
-  ensure
-    RPi::GPIO.clean_up
-  end
+    private
 
-  private
+    def store_state(state)
+      dynamodb.put_item({
+                          item: { 'door' => 'front_door',
+                                  'lock_state' => "#{state}",
+                                  'updated_at' => "#{Time.now}" },
+                          table_name: 'door_lock'
+                        })
+    end
 
-  def store_state(state)
-    dynamodb.put_item({
-                        item: { 'door' => 'front_door',
-                                'lock_state' => "#{state}",
-                                'updated_at' => "#{Time.now}" },
-                        table_name: 'door_lock'
-                      })
-  end
-
-  def dynamodb
-    Aws::DynamoDB::Client.new region: 'eu-central-1', access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-                              secret_access_key: ENV['AWS_ACCESS_KEY']
+    def dynamodb
+      Aws::DynamoDB::Client.new region: 'eu-central-1', access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                                secret_access_key: ENV['AWS_ACCESS_KEY']
+    end
   end
 end
